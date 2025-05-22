797c91b064e98e6de6b4d21446569688f965e2f7
AsrarUddin <asraruddin000@gmail.com>
2025-05-22 12:01:04 +0530

    updated hero-section

--- src/components/herosection.jsx
+++ src/components/herosection.jsx
@@ -124,23 +124,8 @@ function HeroSection() {
         </motion.div>
       </div>
 
-      {/* Scroll down indicator */}
-      <a
-        href="#about"
-        className="absolute bottom-10 left-1/2 transform -translate-x-1/2 text-blue-400 animate-bounce"
-        aria-label="Scroll down to About section"
-      >
-        <svg
-          className="w-8 h-8"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth={2}
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
-        </svg>
-      </a>
+     
+        
     </section>
   );
 }
